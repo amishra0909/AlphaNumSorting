14932f49db0cb2acdc12deddfed898cca57055e3
Abhishek Mishra <abhiusedu@gmail.com>
2014-11-16 03:20:08 -0800

    Base comparator implementation.

---
+++ src/AlphaNumComparator.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+public class AlphaNumComparator implements Comparator<String> {
+
+    // referred to following stackoverflow link to get pattern string:
+    // http://stackoverflow.com/questions/8270784/how-to-split-a-string-between-letters-and-digits-or-between-digits-and-letters
+    private static final Pattern regex = Pattern.compile("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+
+    @Override
+    public int compare(String s1, String s2) {
+
+        String[] s1Sections = regex.split(s1);
+        String[] s2Sections = regex.split(s2);
+
+        int index = 0;
+        while (index < s1Sections.length && index < s2Sections.length) {
+
+            if (s1Sections[index].compareTo(s2Sections[index]) == 0) {
+                index++;
+                continue;
+            }
+            if (isIntSection(s1Sections[index]) && isIntSection(s2Sections[index])) {
+                try {
+                    int s1Integer = Integer.parseInt(s1Sections[index]);
+                    int s2Integer = Integer.parseInt(s2Sections[index]);
+
+                    int diff = s1Integer - s2Integer;
+                    if (diff != 0) {
+                    	return diff;
+                    }
+                    index++;
+                    continue;
+                } catch (NumberFormatException e) {
+                	// naive logging : just put a warning statement on console and throw exception;
+                    System.out.println("ERROR: one of string cannot be parsed as number (execution should not reach here).");
+                    System.out.println(e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            } else {
+                return s1Sections[index].compareTo(s2Sections[index]);
+            }
+
+        }
+
+        // In case when one string is a prefix of other string,
+        // while loop will not fetch a way to compare the strings.
+        // Thus length of the string will determine the order.
+        return s1.length() - s2.length();
+    }
+
+    private boolean isIntSection(String s) {
+        return (s.charAt(0) >= '0' && s.charAt(0) <= '9');
+    }
+
+    // Driving main
+    public static void main(String[] args) {
+        String[] arr = {"a1.txt", "a2.txt", "a10.txt", "a11.txt", "a20.txt", "a3.txt", "11.txt"};
+        Arrays.sort(arr, new AlphaNumComparator());
+        System.out.println(Arrays.toString(arr));
+    }
+}
